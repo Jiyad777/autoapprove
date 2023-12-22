985a2af09b7fec12a846b5e60ff385891ca51590
Jiyad777 <githubcheck7@gmail.com>
2023-12-22 21:22:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ async def autoapprove(c, m):
         button = [[
             InlineKeyboardButton('🎬JOIN MOVIE CHANNEL🎬', url='https://t.me/+9JsGhjucwsozYjdl')
             ],[
-            InlineKeyboardButton('💎JOIN MOVIE GROUP📽️', url='https://t.me/+IibT56jM6gU2NzZl')
+            InlineKeyboardButton('💎JOIN MOVIE GROUP📽️', url='https://t.me/jkmovies_grp')
         ]]
         markup = InlineKeyboardMarkup(button)
         caption = f'Hello {m.from_user.mention()}\nYou Request To Join {m.chat.title} Was Approved.'
@@ -48,7 +48,7 @@ async def start(c, m):
 I'm an auto approve Admin Join Requests Bot.
 I can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.'''
     button = [[
-            InlineKeyboardButton('💎JOIN CHANNEL✅️', url='https://t.me/+f16LP7YW2G03MzM1')
+            InlineKeyboardButton('💎JOIN CHANNEL✅️', url='https://t.me/jk_movies_area')
             ],[
             InlineKeyboardButton('📣UPDATES🔔', url='https://t.me/+uyvbk8Lq6YwxYzM1')
     ]]
